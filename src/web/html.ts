502d322a1d54fe0c3259c8bfd58974714f816838
Chandan Kumar <ck.zero.dev@gamil.com>
2024-07-02 14:45:10 +0530

    feat: implement maya js

---
+++ src/web/html.ts
@@ -0,0 +1,21 @@
+export const getDefaultHtml = (innerHtml: string): string => {
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"
+    />
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <script src="main.js" defer></script>
+    ${innerHtml}
+  </body>
+</html>
+`;
+};
